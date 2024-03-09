c63849b653c596f36b93c8bb57c0a932f87e3769
Duckos-Mods <businwes@hotmail.com>
2024-03-09 14:31:51 +0000

    Added New Macros

--- src/Internal/Defines.hpp
+++ src/Internal/Defines.hpp
@@ -2,6 +2,7 @@
 #include <assert.h>
 #include <exception>
 #include <stdexcept>
+#include <immintrin.h>
 
 #ifndef DXOB_ASSERT
 #define DXOB_ASSERT(x) assert(x)
@@ -19,4 +20,24 @@
 
 #ifndef BYTE_SIZE
 #define BYTE_SIZE sizeof(u8) * 8
+#endif
+
+#ifndef BIT_SIZE
+#define BIT_SIZE(x) (sizeof(x) * 8)
+#endif
+
+#ifndef CLIP_TOP_BIT
+#define CLIP_TOP_BIT(T) T & ~(1 << (sizeof(decltype(T)) * 8 - 1))
+#endif
+
+#ifndef CLIP_BOTTOM_BIT
+#define CLIP_BOTTOM_BIT(T) T & ~(1)
+#endif
+
+#ifndef SZOV
+#define SZOV(x) (sizeof(decltype(x)))
+#endif
+
+#ifndef DISABLE_SIMD
+#define DXOB_USING_SIMD
 #endif
