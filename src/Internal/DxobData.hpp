8a279a1bedb3afc0febce32786d86da425b281de
Duckos-Mods <businwes@hotmail.com>
2024-03-09 14:30:11 +0000

    Added Some more Functions

--- src/Internal/DxobData.hpp
+++ src/Internal/DxobData.hpp
@@ -9,19 +9,23 @@ namespace Dxob
     class HeightDataAccessor
     {
         public:
-            HeightDataAccessor(std::vector<u16> heightData, FileSettings fileSettings) : m_fileSettings(fileSettings), m_heightData(heightData) {}
+            HeightDataAccessor(const std::vector<u16>& heightData, FileSettings fileSettings) : m_fileSettings(fileSettings), m_heightData(heightData) {}
 			HeightDataAccessor() {}
 
 
 
             FileSettings GetFileSettings() const { return m_fileSettings; }
-            std::vector<u16> GetHeightData() const { return m_heightData; }
+            //std::vector<u16> GetHeightData() const { return m_heightData; }
             FileSettings& GetFileSettings() { return m_fileSettings; }
             std::vector<u16>& GetHeightData() { return m_heightData; }
-
-
-
+            std::vector<u16>* GetHeightDataPtr() { return &m_heightData; }
+            bool IsGzip() const { return m_fileSettings.isGZipCompressed; }
+            void SetGzip(bool gzip) { m_fileSettings.isGZipCompressed = gzip; }
             void SetPositionTranslationFunction(PositionTranslationFunction function) { m_positionTranslationFunction = function; }
+            void SetFileSettings(FileSettings settings) { m_fileSettings = settings; }
+            u16 HeightAt(i64 x, i64 y) const;
+
+            void SetHeightAt(i64 x, i64 y, u16 value);
         private:
             PositionTranslationFunction m_positionTranslationFunction = nullptr;
 
