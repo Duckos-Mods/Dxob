e44c9d43a47d37de702b27da1c2ac7dee11d198c
Duckos-Mods <businwes@hotmail.com>
2024-03-09 14:29:57 +0000

    Removed a Field

--- src/Internal/DxobSettings.hpp
+++ src/Internal/DxobSettings.hpp
@@ -7,11 +7,10 @@ namespace Dxob
     // The layout looks ugly, but it's better than having 10 bytes of padding
     struct FileSettings
     {
-        u32 DxobVersion;
-        bool usesPerRowJumps;
-        bool isGZipCompressed;
-        u64 height;
-        u64 width;
-        u64 dataLength;
+        u32 DxobVersion = '0001'; // should be constant i dont think im going to change it
+        bool usesPerRowJumps = false;
+        bool isGZipCompressed = false;
+        u64 height = 0;
+        u64 width = 0;
     };
 }
