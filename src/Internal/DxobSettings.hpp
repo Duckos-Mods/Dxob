a4e4348a1bb5af332b7da650006a338709044630
Duckos-Mods <businwes@hotmail.com>
2024-03-13 17:53:15 +0000

    le const static

--- src/Internal/DxobSettings.hpp
+++ src/Internal/DxobSettings.hpp
@@ -7,7 +7,7 @@ namespace Dxob
     // The layout looks ugly, but it's better than having 10 bytes of padding
     struct FileSettings
     {
-        u32 DxobVersion = '0001'; // should be constant i dont think im going to change it
+        const static u32 DxobVersion = '0001'; // should be constant i dont think im going to change it
         bool usesPerRowJumps = false;
         bool isGZipCompressed = false;
         u64 height = 0;
